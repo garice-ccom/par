eeeb0ba6fc53bae602d1ff2f95c7afa81c6e922b
Mixopteryx <Mixopteryx@users.noreply.github.com>
2016-08-12 12:40:05 -0400

    Restored licence, line break issues,some bugfixes (#3)
    * Add files via upload
    
    Proposed additions and improvements from Tor Inge.
    
    * Fixed complaining on line breaks
    
    * Restored license change
    
    * Found error in file name generation. Missed a spot where the file extension is removed when generating map/nav filename
    
    * Made it possible to use
    while allread(file):
      --- do stuff ---
    
    Automatically closes file when done or on error
    
    * Added support for numeric values in recordlist passed to getping.
    
    * Line ending issues

--- par.py
+++ par.py
@@ -2,28 +2,30 @@
 par.py
 G.Rice 6/20/2012
 
-Copyright (c) 2016 Glen Rice
+This is free and unencumbered software released into the public domain.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
 
-V0.3.3 20160418
-
+V0.3.4 20160418
 This module includes a number of different classes and methods for working with
 Kongsberg all files, each of which are intended to serve at least one of three
 purposes.  These are
@@ -130,15 +132,31 @@ class allRead:
         """
         self.infile.close()
         if clean:
-            mapfilename = self.infilename[:-3] + 'par'
-            navfilename = self.infilename[:-3] + 'nav'
+            mapfilename = self.infilename + '.par'
+            navfilename = self.infilename + '.nav'
             try:
                 os.remove(mapfilename)
                 os.remove(navfilename)
             except:
                 pass
-            
-        
+
+    def __enter__(self):
+        """
+        Start function for with statement
+        Now this will work:
+        with allRead(file):
+                 ....
+
+        The file will be closed automatically
+        """
+        return self
+
+    def __exit__(self,*args):
+        """
+        Exit function for with statement
+        """
+        self.close()
+
     def read(self):
         """
         Reads the header.
@@ -798,6 +816,7 @@ class allRead:
                 if '107' in filerecords:
                     recordlist.append('107')
             else:
+                recordlist = [str(n) for n in recordlist] # Support records as numbers
                 for n in recordlist:
                     if n not in filerecords:
                         recordlist.remove(n)
