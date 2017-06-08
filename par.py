43f5ddff007b4bd584999a65d9860008df856930
Mixopteryx <Mixopteryx@users.noreply.github.com>
2017-06-08 17:37:25 +0200

    Improvement of water column plots (#6)
    * Bugfix: Changed "none" to "None"
    
    * Change initialization of matrices so they are set to nan instead of 0. This avoid the "0 dB" amplitude area below the bottom.

--- par.py
+++ par.py
@@ -327,7 +327,10 @@ class allRead:
             recordsremaining.pop(ping.header['Datagram#']-1)
             totalsamples, subbeams = ping.ampdata.shape
             rx = np.zeros(numbeams, dtype = Data107.nrx_dtype)
-            ampdata = np.zeros((totalsamples, numbeams), dtype = np.float32)
+            # Initialize array to NANs. Source:http://stackoverflow.com/a/1704853/1982894
+            ampdata = np.empty((totalsamples, numbeams), dtype = np.float32)
+            ampdata.fill(np.NAN)
+
             rx[:subbeams] = ping.rx
             ampdata[:,:subbeams] = ping.ampdata
             beamcount = subbeams
@@ -338,7 +341,8 @@ class allRead:
                     recordsremaining.pop(recordnumber)
                     numsamples, subbeams = ping.ampdata.shape
                     if numsamples > totalsamples:
-                        temp = np.zeros((numsamples - totalsamples, numbeams), dtype = 'b')
+                        temp = np.empty((numsamples - totalsamples, numbeams), dtype = np.float32)
+                        temp.fill(np.NAN)
                         ampdata = np.append(ampdata, temp, axis = 0)
                         totalsamples = numsamples
                     rx[beamcount:beamcount+subbeams] = ping.rx
@@ -2423,7 +2427,9 @@ class Data107:
         # declare amplitudes stuff
         numamp = len(datablock) - ntx_sz * ntx - nrx_sz * nrx
         amp_dtype = np.dtype([('SampleAmplitude',"b")])
-        tempamp = np.zeros(numamp, dtype = amp_dtype)
+        # Initialize array to NANs. Source:http://stackoverflow.com/a/1704853/1982894
+        tempamp = np.empty(numamp, dtype = amp_dtype)
+        tempamp[:] = np.NAN
         # get the tx data
         self.tx = np.frombuffer(datablock[:ntx*ntx_sz], dtype = ntx_dtype)
         p = ntx*ntx_sz
@@ -2446,8 +2452,8 @@ class Data107:
         self.rx['BeamPointingAngle'] *= 0.01
         # unwined the beam data into an array
         numsamples = self.rx['NumberSamples']
-        self.ampdata = np.zeros((numsamples.max(), nrx), dtype = np.float32)
-        self.ampdata[:,:] = np.nan
+        self.ampdata = np.empty((numsamples.max(), nrx), dtype = np.float32)
+        self.ampdata[:] = np.NAN
         pamp = 0
         for n in range(nrx):
             self.ampdata[:numsamples[n],n] = 0.5*tempamp[pamp:pamp+numsamples[n]].astype(np.float32)
@@ -4880,7 +4886,8 @@ def _assign_speed_bins(speeds, speed_bins, diff_from_bin = 0.5):
             used_speeds.append(sb)
     # find the indicies where accelerations were lower.
     return np.array(sb_idx), used_speeds
-    
+
+
 def summarize_directory(directory = '.'):
     """
     Print a summary of all the lines in the directory.
@@ -4898,7 +4905,7 @@ def summarize_directory(directory = '.'):
             info[n] = infile.get_stats(False)
         return info
     else:
-        return none
+        return None
 
 def _checksum_all_bytes(bytes):
     # Calculate checksum by sum of bytes method
